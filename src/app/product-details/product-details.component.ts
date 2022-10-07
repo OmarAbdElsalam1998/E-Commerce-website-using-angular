da79d16f0acc47d175c259c4c96ab88d4045c646
OmarAbdElsalam1998 <O.Abdulsalam16.121@compit.au.edu.eg>
2022-10-07 21:47:29 +0200

    updte project

--- src/app/product-details/product-details.component.ts
+++ src/app/product-details/product-details.component.ts
@@ -10,6 +10,30 @@ export class ProductDetailsComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+  //   $(function (){
+  //     'use strict';
+  //    $(".product-images .image img").each(image => {
+  //     image.on("click",function(){
+
+  //     });
+      
+  //    });      
+  // });
+    var images=document.querySelectorAll(".product-images .image img");
+var mainImage=document.querySelector(" .main-image img");
+alert("ass");
+// images.forEach(image=>{
+//     console.log("ima")
+//     image.addEventListener("click",function(){
+//         var src=this.getAttribute("src");
+//         mainImage.setAttribute('src',src);
+//         var list=this.parentNode.parentNode.children;
+//         for(let child of list){
+//             child.classList.remove('active');
+//         }
+//         this.parentNode.classList.add('active');
+//     });
+// })
   }
 
 }
