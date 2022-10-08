959f4ea162028b5eaafab430e1fd63dd4f063e25
Marina Nashaat <marinanashaat642@gmail.com>
2022-10-08 18:09:41 +0200

    update

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
 import { Title } from '@angular/platform-browser';
 
 @Component({
@@ -8,10 +9,30 @@ import { Title } from '@angular/platform-browser';
 })
 export class LoginComponent implements OnInit {
   title="Sign IN";
-  constructor(private titleService:Title) { }
+  constructor(private titleService:Title , private fB: FormBuilder) { }
 
   ngOnInit(): void {
     this.titleService.setTitle(this.title);
   }
+
+  registerationForm =this.fB.group(
+    {
+      userName:['',[Validators.required,Validators.maxLength(32),Validators.minLength(3)]],
+      userPassword:['',[Validators.required,Validators.pattern]],
+
+    }
+  );
+
+  get userName()
+  {
+    return this.registerationForm.get('userName')
+  }
+
+  get userPassword()
+  {
+    return this.registerationForm.get('userPassword')
+
+  }
+
 
 }
