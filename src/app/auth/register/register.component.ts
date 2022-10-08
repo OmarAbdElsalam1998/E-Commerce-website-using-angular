f63cbac65800413528532541c802782786b9632a
Marina Nashaat <marinanashaat642@gmail.com>
2022-10-08 20:44:01 +0200

    new update

--- src/app/auth/register/register.component.ts
+++ src/app/auth/register/register.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+import { ConfirmPasswordValidator } from '../validators/confirmpassword';
 
 @Component({
   selector: 'app-register',
@@ -7,9 +9,42 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
+  constructor(private formB : FormBuilder) { }
 
   ngOnInit(): void {
   }
+
+  registerationForm =this.formB.group(
+    {
+      username:['',[Validators.required,Validators.maxLength(32),Validators.minLength(5)]],           //call simple validators
+      email:['',[Validators.required,Validators.pattern]],
+      password:['',[Validators.required,Validators.pattern]],
+      confirmpassword:['',Validators.required],
+
+    },
+    {validator:[ConfirmPasswordValidator]}               //call Cross field validators (on all form Group not only control)
+
+  );
+
+  get userName()
+  {
+    return this.registerationForm.get('username')
+  }
+
+   get userEmail()
+  {
+    return this.registerationForm.get('email')
+  }
+
+  get Password()
+  {
+    return this.registerationForm.get('password')
+  }
+
+  get confirmPassword()
+  {
+    return this.registerationForm.get('confirmpassword')
+  }
+
 
 }
