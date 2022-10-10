1c0655e10b9b8baff7c0a967c3d584b1118761b8
OmarAbdElsalam1998 <O.Abdulsalam16.121@compit.au.edu.eg>
2022-10-10 14:31:27 +0200

    omar update project

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -8,15 +8,33 @@ import { UserAuthService } from '../services/user-auth.service';
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent implements OnInit {
-  isLoggedIn:boolean;
-  constructor(private userauth:UserAuthService,private router:Router) { 
-    this.isLoggedIn=true;
+  isLoggedIn:any;
+  username:any;
+  constructor(private userauth:UserAuthService,private router:Router,private userAuth:UserAuthService) { 
+    this.isLoggedIn=this.userAuth.getLoggedStatus();
+    console.log(this.username)
+   
   }
     
   ngOnInit(): void {
     this.userauth.getLoggedStatus().subscribe(status=>{
       this.isLoggedIn=status;
     })
+    this.userauth.getUsername().subscribe(status=>{
+      this.username=status;
+    })
+   
+  }
+  ngAfterViewInit(): void {
+    //Called after ngAfterContentInit when the component's view has been initialized. Applies to components only.
+    //Add 'implements AfterViewInit' to the class.
+    
+  }
+
+  logOut(){
+    this.userauth.logOut();
+    this.router.navigate([""]);
+
   }
   search(keyword:any){
      this.router.navigate(['/search/'+keyword.value]);
